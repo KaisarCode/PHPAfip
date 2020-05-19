e9baf0ff2a813d916fbddd75216759bb1a3718e5
KaisarCode <kaisar@kaisarcode.com>
2020-05-19 07:08:37 -0300

    Remove readme from file

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # PHPAfip
 Clase conectora con los servicios de Afip.
 
+Este software es de dominio público, bajo licencia "The Unlicense".
+Esto significa que puede extenderlo a gusto y utilizarlo
+en cualquier proyecto, sea comercial o no.
+Para más información revise la licencia que acompaña este archivo, o
+diríjase a https://unlicense.org/
+
 Esta clase incluye los servicios más utilizados:
 - Facturación electrónica A, B, C, E, T, etc...
 - Consultas de padrón tributario.
@@ -17,9 +23,3 @@ https://www.afip.gob.ar/ws/documentacion/
 
 Para conocer las definiciones de más servicios, diríjase a:
 https://www.afip.gob.ar/ws/documentacion/catalogo.asp
-
-Este software es de dominio público, bajo licencia "The Unlicense".
-Esto significa que puede extenderlo a gusto y utilizarlo
-en cualquier proyecto, sea comercial o no.
-Para más información revise la licencia que acompaña este archivo, o
-diríjase a https://unlicense.org/
--- afip.php
+++ afip.php
@@ -3,23 +3,6 @@
 # PHPAfip - 2019 KaisarCode.com
 Clase conectora con los servicios de Afip.
 
-Esta clase incluye los servicios más utilizados:
-- Facturación electrónica A, B, C, E, T, etc...
-- Consultas de padrón tributario.
-
-Además se agregan 2 extras:
-- Bonos fiscales
-- Seguros de caución
-
-Y la seguiré actualizando con el tiempo.
-(También se aceptan contribuciones).
-
-Para conocer más acerca de los servicios de Afip, diríjase a:
-https://www.afip.gob.ar/ws/documentacion/
-
-Para conocer las definiciones de más servicios, diríjase a:
-https://www.afip.gob.ar/ws/documentacion/catalogo.asp
-
 Este software es de dominio público, bajo licencia "The Unlicense".
 Esto significa que puede extenderlo a gusto y utilizarlo
 en cualquier proyecto, sea comercial o no.
@@ -53,6 +36,7 @@ class Afip
         $this->crt_file = $opts->crt_file;
         $this->crt_pass = $opts->crt_pass;
         $this->cuit = $opts->cuit;
+        $this->cuit = str_replace('-', '', $this->cuit);
         
         // URLs
         $this->url = new stdClass();
